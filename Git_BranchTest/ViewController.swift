b82e4323d9bb05a0c89ee37273e0802a2a1e3595
D7703_18 <d7703_18@D7703-18ui-iMac.local>
2017-09-11 14:43:41 +0900

    add Prinmt dfasd

--- Git_BranchTest/ViewController.swift
+++ Git_BranchTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Git Test")
-        asdfdsa
+        print("sdfjaklf")
     }
 
     override func didReceiveMemoryWarning() {
