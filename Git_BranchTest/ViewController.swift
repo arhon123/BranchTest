4a4245a5c787b9c19dcf7b5f2cb847b3e3e1a099
D7703_18 <d7703_18@D7703-18ui-iMac.local>
2017-09-11 14:48:52 +0900

    Add print

--- Git_BranchTest/ViewController.swift
+++ Git_BranchTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Git Test")
         print("sdfjaklf")
+        print("dsafkjlsa")
     }
 
     override func didReceiveMemoryWarning() {
